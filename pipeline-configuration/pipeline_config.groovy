9969d6c94a27db13701a5a2ae7b186e2d74e73f9
steven-terrana <steventerrana@gmail.com>
2020-04-06 09:05:44 -0400

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,5 +1,13 @@
 allow_scm_jenkinsfile = false 
 
 libraries{
+  sdp{
+    images{
+      registry = "https://docker.pkg.github.com"
+      repository = "boozallen/sdp-images"
+      cred = "github"
+    }
+  }
   pytest
+  owasp_dep_check
 }
