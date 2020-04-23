8ac16150d96f2f0de43f595323b0ce0d58941c72
steven-terrana <steventerrana@gmail.com>
2020-04-23 14:23:00 -0400

    Update policy.tf

--- security-as-code/policy.tf
+++ security-as-code/policy.tf
@@ -3,10 +3,10 @@ resource "sysdig_secure_policy" "terminal_shell_or_ssh_in_container" {
   description = "Detects a terminal shell or a ssh spawned in a container"
   enabled     = true
   severity    = 0 // HIGH
-  scope       = "container.id != \"\""
+  scope       = "kubernetes.cluster.name = 'BAH_CLUSTER' and kubernetes.namespace.name = 'demo'"
   rule_names  = [sysdig_secure_rule_process.terminal_shell.name]
   actions {
-    container = "stop"
+    container = "notify"
     capture {
       seconds_before_event = 5
       seconds_after_event  = 10
