70e3e6dbcb2d7b344f4888baaba20e6c1751921c
steven-terrana <steventerrana@gmail.com>
2020-04-23 14:23:25 -0400

    Update policy.tf

--- security-as-code/policy.tf
+++ security-as-code/policy.tf
@@ -6,7 +6,6 @@ resource "sysdig_secure_policy" "terminal_shell_or_ssh_in_container" {
   scope       = "kubernetes.cluster.name = 'BAH_CLUSTER' and kubernetes.namespace.name = 'demo'"
   rule_names  = [sysdig_secure_rule_process.terminal_shell.name]
   actions {
-    container = "notify"
     capture {
       seconds_before_event = 5
       seconds_after_event  = 10
